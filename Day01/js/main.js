dccec120372f32118930c102e6536c7f663e7c43
ChungNotWibu <h27102006@gmail.com>
2022-10-29 20:50:01 +0700

    2

---
+++ Day01/js/main.js
@@ -0,0 +1,66 @@
+// var modal = document.getElementById('id01');
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+
+
+// bước 1:
+// const btnQuerry = document.querySelector('#btn');
+// const Kiara = document.querySelector('#btn');
+// const K = document.querySelector('#btn');
+// const btnQuerry = document.querySelector('#btn');
+
+// // 
+
+// let collectinfo = {
+//     name:'',
+//     age:'',
+//     school:'',
+//     exp:'',
+
+// }
+// b2  dom event khi user click chuột = on + name event
+// b3 viết function cho sự kiện đã điền ở dom event 2
+function handLeGetInfo(){
+    console.log('worked')
+
+    const promptName = prompt("Your Name");
+    const promptAge = prompt("Your Age");
+    const promptSchool = prompt("Your School");
+    const promptExp = prompt("Your Name");
+}
+
+collectinfo.name = promptName;
+collectinfo.age = promptAge;
+collectinfo.school = promptSchool;
+collectinfo.exp = promptExp;
+
+
+
+
+// b1 lấy khối div ở trong DOM
+const divQuery = document.querySelector("#b-2");
+const DEFAULT_CONTENT = "default";
+// onmouseover, onmouseout
+// b3 viết function
+
+
+function OnMouseOver(params){
+    divQuery.innerText = "Aaaa" 
+}
+
+function OnMouseOut(params){
+    divQuery.innerText = "bbbbb" 
+    
+}
+
+
+
+
+
+
+
+
