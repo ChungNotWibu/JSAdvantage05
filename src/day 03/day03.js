8ae790b4acf587b07b60283ff44d5c2f35c7c075
ChungNotWibu <h27102006@gmail.com>
2022-11-12 19:39:40 +0700

    lkl

--- src/day 03/day03.js
+++ src/day 03/day03.js
@@ -1,6 +1,14 @@
-const arr = [1 , 2 , 3 , 4 , 5 , 7 , 8 , 9]
-const arrx2 = arr.map(
-    (item, index) => {
-        return item = item * 2
+// const arr = [1 , 2 , 3 , 4 , 5 , 7 , 8 , 9]
+// const arrx2 = arr.map(
+//     (item, index) => {
+//         return item = item * 2
+//     }
+// )
+
+const bigArr = [10,11,12,13,14,15,16,17,18,19]
+const ArrDivisor2 = bigArr.map(
+    (item,index) => {
+        return item = item%2 == 0
     }
 )
+console.log(ArrDivisor2)
