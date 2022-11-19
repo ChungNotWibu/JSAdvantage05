9c7bb53eb55d9f06039b09dd0a18e4e5d501999d
ChungNotWibu <h27102006@gmail.com>
2022-11-19 19:40:47 +0700

    ákjdkjd

--- src/day04/main.js
+++ src/day04/main.js
@@ -1,8 +1,8 @@
 // cắt sửa xóa
 
-const myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
-const newFish = myFish.splice(2, 2, 'drum');
-console.log(myFish)
+// const myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+// const newFish = myFish.splice(2, 2, 'drum');
+// console.log(myFish)
 
 // 2 = vị trí bắt đầu - tại mandarin
 // 0 = số lượng muốn xóa , vì bằng 0 nên ko xóa cái nào
@@ -15,4 +15,14 @@ console.log(myFish)
 //  = lượng tham số ta truyền vào , vị trí bắt đầu từ trái qua phải , bắt đầu như trực Ox
 // hàm splice tìm tham số thứ 3 ta truyền vào , tiến hành thêm vào index === index tại bước 1
 
+// js là 1 ngôn ngữ bất đồng bộ
+// jsb chx làm việc với promise : js chạy từ trên xuống dưới, từ trái qua phải
+
+
+console.log("dòng 1")
+console.log("dòng 2")
+
+// thường dòng 1 hiện trc
+// còn trường hợp hiện dòng 2 trước dòng 1:
+// -> xử lí bất đồng bộ của js được áp dụng ,
 
