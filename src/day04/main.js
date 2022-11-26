8171b74d1aeff62bc2670cc84d5f02022c66305e
ChungNotWibu <h27102006@gmail.com>
2022-11-26 19:15:18 +0700

    haha

--- src/day04/main.js
+++ src/day04/main.js
@@ -47,3 +47,95 @@ setTimeout(() => {
 
 // bài toán thực tế:(Buổi 9): net-working : thay vì hard-code data -> lấy data back-end trả về và render ra màn hình
 // trường hợp 1: internet với tốc độ nhanh -> độ trễ thấp 
+
+// const FakeData = [
+//     {
+//         id : 1,
+//         name : "Messi" ,
+//     },
+//     {
+//         id : 2,
+//         name : "Neymar" ,
+//     },
+//     {
+//         id : 3,
+//         name : "Salah" ,
+//     },
+//     {
+//         id : 4,
+//         name : "Harry Mắc Hài" ,
+//     },
+// ];
+
+// setTimeout(() => {FakeData.map(
+//     (item,index) => {
+//          let divChild = document.createElement('div');
+//          divChild.innerText =  item.name;
+//          ListRenderQuery.appendChild(divChild);
+        
+
+//     }
+// )
+// } , 3000)
+
+
+// const divTextQuery = document.querySelector(".div-test")
+
+
+// setInterval(() => {
+    
+//     divTextQuery.classList.toggle("blue-test")   
+// }, 1000);
+
+// setInterval(() => {
+    
+//     divTextQuery.classList.toggle("green-test")   
+// }, 2000);
+// console.log(divTextQuery)
+
+// prope j j đó:
+// add() : thêm
+// remove() : xóa
+//  contanin() : ktra
+// toggle() : nvu gần giống add, nghĩa là thay đổi ; ứng dụng giống như nút bấm của đèn học
+
+// const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+// function isPrime(num) {
+//   for (let i = 2; num > i; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return num > 1;
+// }
+
+// console.log(array.filter(isPrime))
+// const arr = [1, 2, 3, 4, 5, 6]; 
+// const oneAdded = arr.map(num => num + 1); console.log(arr);
+// const myAwesomeArray = [1, 2, 3, 4, 5]
+
+// myAwesomeArray.reduce((total, value) => total * value, 10)
+// console.log(myAwesomeArray)
+// const myAwesomeArray = [[1, 2], [3, [4, 6]], 5]
+// const flat1 = myAwesomeArray.flat()
+// const flat2 = flat1.flat()
+
+// console.log(flat2)
+
+// const myAwesomeArray = [
+//     { id: 1, name: "Hải" },
+//     { id: 2, name: "Doanh" },
+//     { id: 3, name: "Việt" },
+//     { id: 4, name: "Doanh" },
+//   ]
+  
+//   const finalFilter = myAwesomeArray.filter(element => element.name === "Doanh")
+//   console.log(finalFilter)
+// const myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+// const removed = myFish.splice(-2, 1);
+// console.log(myFish)
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6)
+console.log(result)
